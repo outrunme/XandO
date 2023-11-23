4bcc4bc9246084231436c1f2b23f742b17f4b28e
outrunme <148824509+outrunme@users.noreply.github.com>
2023-11-23 22:24:34 +0000

    Add files via upload

--- compAIs.py
+++ compAIs.py
@@ -1,1 +1,107 @@
+import random
 
+
+def random_choice(open_spots):
+    chosen = random.choice(open_spots)
+    return chosen
+
+
+def algorithmic_choice(used_spaces, open_spaces, computer_spaces, player_spaces):
+    chosen = random.choice(open_spaces)
+    if {1, 2}.issubset(computer_spaces) and (not {3}.issubset(used_spaces)):
+        chosen = 3
+    elif {4, 5}.issubset(computer_spaces) and (not {6}.issubset(used_spaces)):
+        chosen = 6
+    elif {7, 8}.issubset(computer_spaces) and (not {9}.issubset(used_spaces)):
+        chosen = 9
+    elif {1, 4}.issubset(computer_spaces) and (not {7}.issubset(used_spaces)):
+        chosen = 7
+    elif {2, 5}.issubset(computer_spaces) and (not {8}.issubset(used_spaces)):
+        chosen = 8
+    elif {3, 6}.issubset(computer_spaces) and (not {9}.issubset(used_spaces)):
+        chosen = 9
+    elif {1, 5}.issubset(computer_spaces) and (not {9}.issubset(used_spaces)):
+        chosen = 9
+    elif {3, 5}.issubset(computer_spaces) and (not {7}.issubset(used_spaces)):
+        chosen = 7
+    elif {1, 3}.issubset(computer_spaces) and (not {2}.issubset(used_spaces)):
+        chosen = 2
+    elif {4, 6}.issubset(computer_spaces) and (not {5}.issubset(used_spaces)):
+        chosen = 5
+    elif {7, 9}.issubset(computer_spaces) and (not {8}.issubset(used_spaces)):
+        chosen = 8
+    elif {1, 7}.issubset(computer_spaces) and (not {4}.issubset(used_spaces)):
+        chosen = 4
+    elif {2, 8}.issubset(computer_spaces) and (not {5}.issubset(used_spaces)):
+        chosen = 5
+    elif {3, 9}.issubset(computer_spaces) and (not {6}.issubset(used_spaces)):
+        chosen = 6
+    elif {1, 9}.issubset(computer_spaces) and (not {5}.issubset(used_spaces)):
+        chosen = 5
+    elif {3, 7}.issubset(computer_spaces) and (not {5}.issubset(used_spaces)):
+        chosen = 5
+    elif {3, 2}.issubset(computer_spaces) and (not {1}.issubset(used_spaces)):
+        chosen = 1
+    elif {6, 5}.issubset(computer_spaces) and (not {4}.issubset(used_spaces)):
+        chosen = 4
+    elif {9, 8}.issubset(computer_spaces) and (not {7}.issubset(used_spaces)):
+        chosen = 7
+    elif {7, 4}.issubset(computer_spaces) and (not {1}.issubset(used_spaces)):
+        chosen = 1
+    elif {8, 5}.issubset(computer_spaces) and (not {2}.issubset(used_spaces)):
+        chosen = 2
+    elif {9, 6}.issubset(computer_spaces) and (not {3}.issubset(used_spaces)):
+        chosen = 3
+    elif {9, 5}.issubset(computer_spaces) and (not {1}.issubset(used_spaces)):
+        chosen = 1
+    elif {7, 5}.issubset(computer_spaces) and (not {3}.issubset(used_spaces)):
+        chosen = 3
+    elif {1, 2}.issubset(player_spaces) and (not {3}.issubset(used_spaces)):
+        chosen = 3
+    elif {4, 5}.issubset(player_spaces) and (not {6}.issubset(used_spaces)):
+        chosen = 6
+    elif {7, 8}.issubset(player_spaces) and (not {9}.issubset(used_spaces)):
+        chosen = 9
+    elif {1, 4}.issubset(player_spaces) and (not {7}.issubset(used_spaces)):
+        chosen = 7
+    elif {2, 5}.issubset(player_spaces) and (not {8}.issubset(used_spaces)):
+        chosen = 8
+    elif {3, 6}.issubset(player_spaces) and (not {9}.issubset(used_spaces)):
+        chosen = 9
+    elif {1, 5}.issubset(player_spaces) and (not {9}.issubset(used_spaces)):
+        chosen = 9
+    elif {3, 5}.issubset(player_spaces) and (not {7}.issubset(used_spaces)):
+        chosen = 7
+    elif {1, 3}.issubset(player_spaces) and (not {2}.issubset(used_spaces)):
+        chosen = 2
+    elif {4, 6}.issubset(player_spaces) and (not {5}.issubset(used_spaces)):
+        chosen = 5
+    elif {7, 9}.issubset(player_spaces) and (not {8}.issubset(used_spaces)):
+        chosen = 8
+    elif {1, 7}.issubset(player_spaces) and (not {4}.issubset(used_spaces)):
+        chosen = 4
+    elif {2, 8}.issubset(player_spaces) and (not {5}.issubset(used_spaces)):
+        chosen = 5
+    elif {3, 9}.issubset(player_spaces) and (not {6}.issubset(used_spaces)):
+        chosen = 6
+    elif {1, 9}.issubset(player_spaces) and (not {5}.issubset(used_spaces)):
+        chosen = 5
+    elif {3, 7}.issubset(player_spaces) and (not {5}.issubset(used_spaces)):
+        chosen = 5
+    elif {3, 2}.issubset(player_spaces) and (not {1}.issubset(used_spaces)):
+        chosen = 1
+    elif {6, 5}.issubset(player_spaces) and (not {4}.issubset(used_spaces)):
+        chosen = 4
+    elif {9, 8}.issubset(player_spaces) and (not {7}.issubset(used_spaces)):
+        chosen = 7
+    elif {4, 7}.issubset(player_spaces) and (not {1}.issubset(used_spaces)):
+        chosen = 1
+    elif {5, 8}.issubset(player_spaces) and (not {2}.issubset(used_spaces)):
+        chosen = 2
+    elif {6, 9}.issubset(player_spaces) and (not {3}.issubset(used_spaces)):
+        chosen = 3
+    elif {5, 9}.issubset(player_spaces) and (not {1}.issubset(used_spaces)):
+        chosen = 1
+    elif {7, 5}.issubset(player_spaces) and (not {3}.issubset(used_spaces)):
+        chosen = 3
+    return chosen
--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import functions
+import compAIs
+import random
 
 matrix_board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
@@ -23,12 +25,12 @@ def game_x():
     ]
     functions.print_game_state(board)
     options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    used = []
-    player_spots = []
-    computer_spots = []
+    used = set()
+    player_spots = set()
+    computer_spots = set()
     game_over_check = False
     idiocy_count = 0
-    while set(used) < {1, 2, 3, 4, 5, 6, 7, 8, 9}:
+    while used < {1, 2, 3, 4, 5, 6, 7, 8, 9}:
         if not game_over_check:
             while True:
                 digit_check = input("Your move?:")
@@ -47,20 +49,24 @@ def game_x():
                     break
             functions.change_game_state(board, player_choice, "X", used, matrix_board)
             functions.print_game_state(board)
-            used.append(player_choice)
-            player_spots.append(player_choice)
+            used.add(player_choice)
+            player_spots.add(player_choice)
             functions.check(player_spots, "Player")
             game_over_check = functions.update(player_spots, game_over_check)
             if game_over_check:
                 break
             print("")
-        if (set(used) < {1, 2, 3, 4, 5, 6, 7, 8, 9}) and not game_over_check:
-            place = list(set(options) - set(used))
-            computer_choice = functions.comp_ai(place)
+        if (used < {1, 2, 3, 4, 5, 6, 7, 8, 9}) and not game_over_check:
+            open = list(set(options) - used)
+            computer_choice = compAIs.algorithmic_choice(
+                used, open, computer_spots, player_spots
+            )
+            if computer_choice in used:
+                computer_choice = random.choice(open)
             functions.change_game_state(board, computer_choice, "O", used, matrix_board)
             functions.print_game_state(board)
-            used.append(computer_choice)
-            computer_spots.append(computer_choice)
+            used.add(computer_choice)
+            computer_spots.add(computer_choice)
             functions.check(computer_spots, "Computer")
             game_over_check = functions.update(computer_spots, game_over_check)
             if game_over_check:
@@ -88,26 +94,30 @@ def game_o():
     functions.print_game_state(board)
     print("")
     options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    used = []
-    player_spots = []
-    computer_spots = []
+    used = set()
+    player_spots = set()
+    computer_spots = set()
     game_over_check = False
     idiocy_count = 0
-    while set(used) < {1, 2, 3, 4, 5, 6, 7, 8, 9}:
+    while used < {1, 2, 3, 4, 5, 6, 7, 8, 9}:
         if not game_over_check:
-            place = list(set(options) - set(used))
-            computer_choice = functions.comp_ai(place)
+            open = list(set(options) - used)
+            computer_choice = compAIs.algorithmic_choice(
+                used, open, computer_spots, player_spots
+            )
+            if computer_choice in used:
+                computer_choice = random.choice(open)
             functions.change_game_state(board, computer_choice, "X", used, matrix_board)
             functions.print_game_state(board)
-            used.append(computer_choice)
-            computer_spots.append(computer_choice)
+            used.add(computer_choice)
+            computer_spots.add(computer_choice)
             functions.check(computer_spots, "Computer")
             game_over_check = functions.update(computer_spots, game_over_check)
             if game_over_check:
                 break
             print("")
 
-        if (set(used) < {1, 2, 3, 4, 5, 6, 7, 8, 9}) and not game_over_check:
+        if (used < {1, 2, 3, 4, 5, 6, 7, 8, 9}) and not game_over_check:
             while True:
                 digit_check = input("Your move?:")
                 if digit_check.isdigit():
@@ -125,8 +135,8 @@ def game_o():
                     break
             functions.change_game_state(board, player_choice, "O", used, matrix_board)
             functions.print_game_state(board)
-            used.append(player_choice)
-            player_spots.append(player_choice)
+            used.add(player_choice)
+            player_spots.add(player_choice)
             functions.check(player_spots, "Player")
             game_over_check = functions.update(player_spots, game_over_check)
             if game_over_check:
